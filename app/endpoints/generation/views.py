c2b88fbca2c038a792b89c9acea047410bbfd72f
JuanAguirre-95 <69450876+JuanAguirre-95@users.noreply.github.com>
2022-06-01 00:44:00 -0300

    Added generator view

---
+++ app/endpoints/generation/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request
+from app.services.generators.generator_service import GeneratorService
+
+gen_view = Blueprint("gen_view", __name__, url_prefix="/generate")
+
+gen = GeneratorService([".password", ".passphrase", ".wallet_btc"])
+
+
+def validate_json_body(body: dict):
+    pass
+
+
+@gen_view.post("/")
+def generate():
+    """
+    Instantiates a generator with the specified parameters that are included in the request body and returns the
+    generated results.
+    :return: JSON
+    """
+    payload = request.get_json()
+    validate_json_body(payload)
+    generator = gen.create_generator(payload)
+    return generator.generate()
+
