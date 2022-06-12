ab1a03ffdac434fbca2dfede90038f9891e95804
JuanAguirre-95 <69450876+JuanAguirre-95@users.noreply.github.com>
2022-06-12 03:36:12 -0300

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,16 +1,36 @@
+import bcrypt
 from flask import Flask
 from app import config
 
+from app.endpoints.generation import gen_view
+from app.endpoints.generators import generators_view
+from app.endpoints.vault import vault_view
+from app.endpoints.auth import login_view
+
+from app.modules import db, jwt
+
 
 def create_app(scope: str = "dev"):
-    flask_app = Flask(__name__)
-    flask_app.config.from_object(config.get_config(scope))
-    from app.endpoints.generation.views import gen_view
+    """Econokey Backend App Factory"""
+    econokey = Flask(__name__)
+    econokey.config.from_object(config.get_config(scope))
 
-    with flask_app.app_context():
-        flask_app.register_blueprint(gen_view)
+    view_list = [gen_view, generators_view, vault_view, login_view]
+    with econokey.app_context():
+        # Register available endpoint blueprints
+        for view in view_list:
+            econokey.register_blueprint(view)
 
-    return flask_app
+        # Initialize SQLAlchemy ORM
+        db.init_app(econokey)
+        db.create_all(app=econokey)
+        db.session.commit()
+
+        # Initialize JWT manager
+        jwt.init_app(econokey)
+
+
+    return econokey
 
 
 app = create_app()
