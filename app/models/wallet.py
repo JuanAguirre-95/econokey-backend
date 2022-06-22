3e071b5ded0012fdb526831364bf2b2157bc605a
JuanAguirre-95 <69450876+JuanAguirre-95@users.noreply.github.com>
2022-06-22 03:35:28 -0300

    Updated wallet model names

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields
 
 class WalletSchema(Schema):
     wallet_name = fields.Str(required=True)
-    token = fields.Str(required=True)
-    wallet_address = fields.Str(required=True)
-    wallet_key = fields.Str(required=True)
-    wallet_passphrase = fields.Str(required=False)
+    cryptocurrency = fields.Str(required=True)
+    public_key = fields.Str(required=True)
+    private_key = fields.Str(required=True)
+    passphrase = fields.Str(required=False)
